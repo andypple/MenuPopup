5c2843636870907b8e6aa82056c09c29f7d9fd35
Phat Le <ltanphat@gmail.com>
2014-07-15 11:27:20 +0700

    Initial commit

---
+++ Menu/OAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OAAppDelegate.h
+//  Menu
+//
+//  Created by Phat, Le Tan on 7/15/14.
+//  Copyright (c) 2014 FourFi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Menu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Menu
+//
+//  Created by Phat, Le Tan on 7/15/14.
+//  Copyright (c) 2014 FourFi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OAAppDelegate class]));
+    }
+}
